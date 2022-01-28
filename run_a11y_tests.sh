35275f67f6d5db2fbcf0524f6ea964096420e21f
JamieNorth <75728919+JamieNorth@users.noreply.github.com>
2022-01-28 16:33:50 +0000

    TODO removes

--- run_a11y_tests.sh
+++ run_a11y_tests.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
+
+sm --stop ASSETS_FRONTEND
+sleep 5
+sm --start ASSETS_FRONTEND -r 4.22.0
+
 sbt -Denvironment=local -Dbrowser=remote-chrome clean 'testOnly uk.gov.hmrc.test.ui.runner.A11yRunner'
+
